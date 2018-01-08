8cfae002a75fb14e925ff971fda319a4878e221e
dylanterrell <dylan.terrell14@stjohns.edu>
2018-01-08 00:02:16 -0500

    Comments

--- chain.js
+++ chain.js
@@ -1,10 +1,15 @@
 const Block = require('./Block.js');
 
+/*
+ * A class that holds our blockchain implemented using an array
+ * Our chain is initialized by a Genesis Block - or the first block in the scratchChain
+ * Currently able to get the most recent block on the chain and also add new block to our chain
+ */
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
   }
-
+  
   createGenesisBlock() {
     const currentDate = new Date();
     return new Block(0, this.date, "Genesis Block", "0");
